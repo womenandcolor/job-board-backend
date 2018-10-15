dddf8e8f471b22c51dc75bb4da9763b8d5702808
Sharon Kennedy <sharon@nomadiclabs.ca>
2018-10-15 00:06:27 -0400

    use connection string for heroku

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,6 @@
 const knex = require("knex")({
   client: "pg",
-  connection: {
-    host: "db",
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB
-  }
+  connection: process.env.DATABASE_URL
 });
 
 module.exports = {
