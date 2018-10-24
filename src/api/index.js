1850f3da4408a51c6624736d65afed80a6ef99da
Sharon Kennedy <sharon@nomadiclabs.ca>
2018-10-23 23:54:55 -0400

    remove legacy routes

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,9 @@
 const express = require("express");
 
 const { healthRouter } = require('../routes/health/health.router')
-const { profilesRouter } = require('../routes/profiles/profiles.router')
 
 const router = express.Router();
 
 router.use("/health", healthRouter);
-router.use("/api/v1", profilesRouter);
 
 module.exports = router;
